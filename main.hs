77b9c81b5d21049a88100db060604c0c255f7314
John Dobronszki <janosdobronszki@citromail.hu>
2013-12-26 16:32:01 +0000

    Playing around

--- main.hs
+++ main.hs
@@ -3,39 +3,129 @@
 
 import Hakit
 import qualified Hakit.Http as H
-import qualified System.Environment as SE  
+-- import qualified System.Environment as SE  
 import qualified Data.List as L
 import qualified Data.Map as M
 import qualified Control.Concurrent as CC
 import qualified Data.Text as T
 import qualified Control.Concurrent.MVar as MV
 import qualified Data.IORef as Ref
+import Debug
 
-data Instance = Instance {
-    lastHeartbeat   :: Integer,
-    name            :: T.Text,
-    endpoints       :: T.Text
-}
+{--------------------------------------------------------------------
+  Types and helpers.  
+--------------------------------------------------------------------}
 
-data ConnectedInstances = ConnInst {
-    nameToNodes :: M.Map T.Text Instance
-} 
+data Endpoint = E {
+    alias :: T.Text,        -- Ie. "register"
+    path :: T.Text,         -- Ie. "whatever/you/wish/reg"
+    method :: T.Text
+} deriving (Eq, Show)
 
-data Test = Test {
-    x :: Integer
-}
+-- Horrible boilerplate, extend hakit to deal with it.
+instance DocValLike Endpoint where
+    toDocVal e = d [
+            "alias"     .- alias e,
+            "path"      .- path e,
+            "method"    .- method e
+        ]
+    fromDocVal d =
+        let doc = toMap d
+        in E    (toString $ get "alias" doc)
+                (toString $ get "path" doc)
+                (toString $ get "method" doc)
+
+data Instance = I {
+    connectTime     :: Integer,                 -- Timestamp when the connection was established
+    address         :: T.Text,                  -- "http://myservice.com" or "https://myservice.com:8081"
+    serviceName     :: T.Text,                  -- Servicename, eg. "image-resize", "comment"
+    instanceName    :: T.Text,                  -- A unique identifier of the service instance
+    endpoints       :: M.Map T.Text Endpoint    -- Map from endpoint alias to Endpoint
+} deriving (Eq, Show)
+
+instance DocValLike Instance where
+    toDocVal i = d [
+            "connectTime"   .- connectTime i,
+            "address"       .- address i,
+            "serviceName"   .- serviceName i,
+            "instanceName"  .- instanceName i,
+            "endpoints"     .- (d . map (\(k, v) -> (k, d v)) . M.toList $ endpoints i)
+        ]
+    fromDocVal d =
+        let doc = toMap d
+        in I    (toInt      $ get "connectTime" doc)
+                (toString   $ get "address" doc)
+                (toString   $ get "serviceName" doc)
+                (toString   $ get "instanceName" doc)
+                (M.fromList . map (\(k, v) -> (k, (fromDocVal v)::Endpoint)) . M.toList . toMap $ get "endpoints" doc)
+
+serviceNames :: Instances -> [T.Text]
+serviceNames ins =
+    let nameList = map fst . M.toList $ nameToNodes ins
+        unique = M.fromList $ map (\x -> (x, ())) nameList 
+    in map fst $ M.toList unique
+
+-- Maps serviceNames to instances
+data Instances = Is {
+    nameToNodes :: M.Map T.Text [Instance]
+} deriving (Eq, Show)
+
+instance DocValLike Instances where
+    toDocVal i = d [
+            "nameToNodes" .- (dm . map (\(k,v) -> (k, d $ map d v)) . M.toList $ nameToNodes i)
+        ]
+
+{--------------------------------------------------------------------
+  Handlers.  
+--------------------------------------------------------------------}
+
+pingHandler = return $ H.setBody pong H.resp
+    where
+        pong = toJSON $ dm ["pong" .- True]
+
+unrecHandler = return $ H.setBody unrecognizedPath H.resp
+    where
+        unrecognizedPath = toJSON $ dm ["error" .- "Unrecognized path"]
+
+connectedHandler :: Ref.IORef Instances -> IO H.Resp
+connectedHandler instances = do
+    inst <- Ref.readIORef instances
+    return $ H.setBody (toJSON . toMap $ toDocVal inst) H.resp
+
+ok = toJSON $ dm ["ok" .- True]
+
+{--------------------------------------------------------------------
+  Connect handler.  
+--------------------------------------------------------------------}
+
+--connectValidator = [
+--        "serverName"    .- True,
+--        "instanceName"  .- True
+--    ]
+
+connectHandler :: Document -> Ref.IORef Instances -> IO H.Resp
+connectHandler dat instances = do
+    _ <- Ref.atomicModifyIORef' instances upd
+    return $ H.setBody ok H.resp
+    where
+        inst :: Instance
+        inst = fromDocVal . d . set "connectTime" 1 $ dat
+        upd :: Instances -> (Instances, ())
+        upd i =
+            let servName = serviceName (trace (show inst) inst)
+            in (Is (M.adjust (\xs -> inst:xs) servName $ nameToNodes i), ())
+
+{--------------------------------------------------------------------
+  Main loop.  
+--------------------------------------------------------------------}
 
 main = do
     putStrLn "Starting ORC"
-    t <- Ref.newIORef $ Test 0
-    let inc :: Test -> (Test, ())
-        inc tes = (Test $ x tes + 1, ())
-    args <- SE.getArgs
+    instances <- Ref.newIORef . Is $ M.fromList []
     let handler req = case H.path req of
-                ["inc"]     -> do
-                        ret <- Ref.readIORef t
-                        _ <- Ref.atomicModifyIORef' t inc
-                        return $ H.setBody (show $ x ret) H.resp
-                ["ping"]    -> return $ H.setBody "pong" H.resp
-                otherwise   -> return $ H.setBody "Unrecognized path" H.resp
+            -- ["disconnect"] -> disconnectHandler instances
+            ["connected"]   -> connectedHandler instances
+            ["connect"]     -> connectHandler (H.params req) instances
+            ["ping"]        -> pingHandler
+            otherwise       -> unrecHandler
     H.startServer 8081 handler
